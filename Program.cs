47caeb74de42d2353e7716543c60464a926b3816
ptiloviola <ptiloviola@gmail.com>
2025-12-22 17:33:59 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        float x = 0.17f;
+
+        if (!Int32.TryParse(Console.ReadLine(), out var a))
+        {
+            Console.WriteLine("Not a number!");
+            return;
+        }
+        if (!Int32.TryParse(Console.ReadLine(), out var b))
+        {
+            Console.WriteLine("Not a number!");
+            return;
+        }
+
+        var s = Console.ReadLine();
+        var boolVar = false;
+        if (s.Length == 0 || s.Length > 1 && !boolVar)
+        {
+            Console.WriteLine("Wrong sign");
+            return;
+        }
+        switch (s[0])
+        {
+            case '+':
+                Console.WriteLine("Result of {0} + {1} = {2}", a, b, a + b);
+                break;
+            case '-':
+                Console.WriteLine("Result of {0} - {1} = {2}", a, b, a - b);
+                break;
+            case '*':
+                Console.WriteLine("Result of {0} * {1} = {2}", a, b, a * b);
+                break;
+            case '/':
+                Console.WriteLine("Result of {0} / {1} = {2}", a, b, a / b);
+                break;
+            case '%':
+                Console.WriteLine("Result of {0} % {1} = {2}", a, b, a % b);
+                break;
+            default:
+                Console.WriteLine("wrong sign");
+                break;
+        }
