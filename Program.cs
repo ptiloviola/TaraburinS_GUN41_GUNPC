eebd59bf2d58bfe371e71cfaddd5fd1766b2ccf9
ptiloviola <ptiloviola@gmail.com>
2026-01-08 20:54:22 +0300

    completed all tasks

--- Program.cs
+++ Program.cs
@@ -10,7 +10,9 @@ class Program
 
         // Task #1
 
-        int[] arrayFib = new int[8];
+        Console.WriteLine("Task #1");
+
+        int[] arrayFib = new int[10];
         for (int i = 0; i < arrayFib.Length; i++)
         {
             if (i < 2)
@@ -21,135 +23,50 @@ class Program
             {
                 arrayFib[i] = arrayFib[i - 1] + arrayFib[i - 2];
             }
+            Console.WriteLine(arrayFib[i]);
         }
-
-        Console.WriteLine("arrayFib = {0}", $"[{string.Join(", ", arrayFib)}]");
 
 
         // Task #2
 
-        var en = new CultureInfo("en-US");
+        Console.WriteLine("Task #2");
 
-        string[] arrayMonts = new string[12];
-
-        for (int i = 0; i < 12; i++)
+        for (int i = 2; i <= 20; i+=2)
         {
-            arrayMonts[i] = en.DateTimeFormat.GetMonthName(i+1);
+            Console.WriteLine(i);
         }
-
-        Console.WriteLine("arrayMonts = {0}", $"[{string.Join(", ", arrayMonts)}]");
 
         // Task #3
 
-        int[,] arrayMatrix = new int[3, 3];
+        Console.WriteLine("Task #3");
 
-        for (int i = 0; i < arrayMatrix.GetLength(0); i++)
+        for (int i = 1; i <= 5; i++)
         {
-            for (int j = 0; j < arrayMatrix.GetLength(1); j++)
+            for (int j = 1; j <= 5; j++)
             {
-                arrayMatrix[i, j] = (int)Math.Pow(j + 2, i + 1);
+                Console.WriteLine("{0} * {1} = {2}", i, j, i * j);
             }
         }
 
-        var arrayMatrixString = new StringBuilder();
+        // Task #4
 
-        arrayMatrixString.Append("[");
+        Console.WriteLine("Task #4");
 
-        for (int i = 0; i < arrayMatrix.GetLength(0); i++)
+        string password = "qwerty";
+        string input = "";
+
+        Console.WriteLine("Enter password");
+
+        do
         {
-            arrayMatrixString.Append("[");
-            for (int j = 0; j < arrayMatrix.GetLength(1); j++)
+            if (input != "")
             {
-                arrayMatrixString.Append(arrayMatrix[i, j]);
-                if (j < arrayMatrix.GetLength(1) - 1)
-                {
-                    arrayMatrixString.Append(", ");
-                } 
-                else
-                {
-                    arrayMatrixString.Append("]");
-                }
+                Console.WriteLine("Incorrect password. Try again!");
             }
-            if (i < arrayMatrix.GetLength(0) - 1)
-            {
-                arrayMatrixString.Append(", ");
-            }
-            else
-            {
-                arrayMatrixString.Append("]");
-            }
-        }
+            input = Console.ReadLine();
+        } while (password != input);
 
-        Console.WriteLine("arrayMatrix = {0}", arrayMatrixString);
-
-        // Task # 4
-
-        double[][] arrayJagged = new double[3][];
-        arrayJagged[0] = new double[5];
-        arrayJagged[1] = new double[2];
-        arrayJagged[2] = new double[4];
-
-        for (int i = 0; i < arrayJagged[0].Length; i++)
-        {
-            arrayJagged[0][i] = i + 1;
-        }
-
-        arrayJagged[1][0] = Math.PI;
-        arrayJagged[1][1] = Math.E;
-
-        for (int i = 0; i < arrayJagged[2].Length; i++)
-        {
-            //arrayJagged[2][i] = i;
-            arrayJagged[2][i] = Math.Log10(Math.Pow(10, i)); 
-        }
-
-
-        var arrayJaggedString = new StringBuilder();
-        arrayJaggedString.Append("[");
-        for (int i = 0; i < arrayJagged.Length; i++)
-        {
-            arrayJaggedString.Append($"[{string.Join(", ", arrayJagged[i])}]");
-            if (i < arrayJagged.Length - 1)
-            {
-                arrayJaggedString.Append(", ");
-            }
-            else
-            {
-                arrayJaggedString.Append("]");
-            }
-        }
-      
-        Console.WriteLine("arrayJagged = {0}", arrayJaggedString);
-
-        // Task #5
-
-        int[] array = { 1, 2, 3, 4, 5 };
-        int[] array2 = { 7, 8, 9, 10, 11, 12, 13 };
-
-        Array.Copy(array, array2, 3);
-
-        Console.WriteLine("array2 = {0}", $"[{string.Join(", ", array2)}]");
-
-        // Task #6
-
-        Array.Resize(ref array, array.Length * 2);
-
-        Console.WriteLine("array = {0}", $"[{string.Join(", ", array)}]");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        Console.WriteLine("Correct password!");
 
         Console.WriteLine("Press any key to exit...");
         Console.ReadKey();
