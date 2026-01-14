0ea7c236ba7f9eb5d4554f8ca4ea4e981548dd56
ptiloviola <ptiloviola@gmail.com>
2026-01-14 15:43:28 +0300

    start solving

--- Program.cs
+++ Program.cs
@@ -1,72 +1,14 @@
-﻿using System.Globalization;
-using System.Runtime.Serialization.Formatters;
-using System.Text;
-
+﻿using Classes;
 
 class Program
 {
     static void Main(string[] args)
     {
 
-        // Task #1
-
-        Console.WriteLine("Task #1");
-
-        int[] arrayFib = new int[10];
-        for (int i = 0; i < arrayFib.Length; i++)
-        {
-            if (i < 2)
-            {
-                arrayFib[i] = i;
-            }
-            else
-            {
-                arrayFib[i] = arrayFib[i - 1] + arrayFib[i - 2];
-            }
-            Console.WriteLine(arrayFib[i]);
-        }
-
-
-        // Task #2
-
-        Console.WriteLine("Task #2");
-
-        for (int i = 2; i <= 20; i+=2)
-        {
-            Console.WriteLine(i);
-        }
-
-        // Task #3
-
-        Console.WriteLine("Task #3");
-
-        for (int i = 1; i <= 5; i++)
-        {
-            for (int j = 1; j <= 5; j++)
-            {
-                Console.WriteLine("{0} * {1} = {2}", i, j, i * j);
-            }
-        }
-
-        // Task #4
-
-        Console.WriteLine("Task #4");
-
-        string password = "qwerty";
-        string input = "";
-
-        Console.WriteLine("Enter password");
-
-        do
-        {
-            if (input != "")
-            {
-                Console.WriteLine("Incorrect password. Try again!");
-            }
-            input = Console.ReadLine();
-        } while (password != input);
-
-        Console.WriteLine("Correct password!");
+        var unit = new Unit();
+        Console.WriteLine(unit.Name);
+        unit.Armour = 15;
+        Console.WriteLine(unit.Armour);
 
         Console.WriteLine("Press any key to exit...");
         Console.ReadKey();
---
+++ Unit.cs
@@ -0,0 +1,49 @@
+﻿namespace Classes
+{
+    public class Unit
+    {
+
+        private float _health;
+        private float _armour;
+
+        public string Name { get; }
+
+        public float Health => _health;
+
+        public Unit() : this("Unknown Unit")
+        {
+
+        }
+
+        public Unit(string name)
+        {
+            Name = name;
+        }
+
+        public float GetRealHealth()
+        {
+            return Health * (1f + Armour);
+        }
+
+        public float Armour
+        {
+            get
+            {
+                return (float)Math.Round(_armour, 2);
+            }
+            set
+            {
+                if (value >= 0 || value <= 1)
+                {
+                    _armour = value;
+                }
+                else
+                {
+                    Console.WriteLine("Error armour value");
+                }
+            }
+        }
+    }
+}
+
+
